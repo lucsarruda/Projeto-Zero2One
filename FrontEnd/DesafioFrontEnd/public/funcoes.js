6db8c43521aecc40fa275cf769b39f9028f9ee83
lucsarruda <lucasarruda3@hotmail.com>
2019-05-28 19:51:58 -0300

    V1 Fontes Desafio

---
+++ FrontEnd/DesafioFrontEnd/public/funcoes.js
@@ -0,0 +1,109 @@
+const concluidas = []
+
+// @loadConcluidas()
+// Funcao para carregar e criar table com tarefas concluidas
+function loadConcluidas() {
+
+    document.getElementById("divItens").innerHTML = ""
+
+    if (concluidas.length === 0) {
+        return
+    }
+
+    let strDiv = `            
+                                                
+            <h2>Concluidas</h2>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Status</th>
+                </tr> `
+
+    for (let index = 0; index < concluidas.length; index++) {
+        strDiv += `
+                <tr>
+                    <td>${concluidas[index].nome}</td>
+                    <td><input type="checkbox" checked disabled></td>
+                </tr>
+                `
+
+    }
+    strDiv += `</table>`
+
+    var div = document.createElement("div");
+    div.setAttribute("class", "item")
+    div.innerHTML = strDiv;
+    document.getElementById("divItens").appendChild(div);
+
+}
+
+// @checarCheck(obj)
+//     - Parametro : obj - Objeto vindo do evento onclik
+// Funcao de verificação de check ou uncheck no evento do input
+function checarCheck(obj) {
+
+    const varobj = obj
+    if (varobj.checked) {
+        concluidas.push({ 'id': varobj.id, 'nome': varobj.value })
+        loadConcluidas()
+    } else {
+        for (let index = 0; index < concluidas.length; index++) {
+            if (concluidas[index].id === varobj.id) {
+                concluidas.splice(index, 1);
+                loadConcluidas()
+                return
+            }
+        }
+
+    }
+
+}
+
+// @CarregaGet()
+// Funcao para chamada de API das tarefas inicias
+function CarregaGet() {
+
+    const URL = 'https://api.myjson.com/bins/jh0yn';
+
+    fetch(URL, {
+            metodo: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(async response => {
+            CarregaTarefasCabec(await response.json())
+        })
+        .catch(error => {
+            console.log(error)
+        });
+
+}
+
+// @CarregaTarefasCabec(json)
+//      - Parametro : json - RETORNO DA API INICIAL
+// Funcao para criação inicial da carga das tarefas
+function CarregaTarefasCabec(json) {
+    document.getElementById("divTarefas").innerHTML = ""
+
+    let strDiv = ''
+
+    for (let index = 0; index < json.length; index++) {
+        let checkvar = ""
+        if (json[index].status) {
+            checkvar = "checked"
+            concluidas.push({
+                'id': json[index].id.toString(),
+                'nome': json[index].nome
+            })
+            loadConcluidas()
+        }
+        strDiv += `                                          
+            <input type="checkbox" class="checks" name="${json[index].id}" id="${json[index].id}" value="${json[index].nome}" onclick="checarCheck(this)" ${checkvar} > ${json[index].nome}<br>
+        `
+    }
+
+    var div = document.createElement("div");
+    div.innerHTML = strDiv;
+    document.getElementById("divTarefas").appendChild(div);
+}
